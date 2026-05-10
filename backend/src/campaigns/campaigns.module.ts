ae04db5a7c7da3d14057fb55075a50a7644de229
Oscar Falero <oscarfalero10@gmail.com>
2026-05-10 02:29:58 +0200

    fix(backend): add missing repository imports in CampaignsModule

--- backend/src/campaigns/campaigns.module.ts
+++ backend/src/campaigns/campaigns.module.ts
@@ -4,9 +4,12 @@ import { CampaignsService } from './campaigns.service';
 import { CampaignsController } from './campaigns.controller';
 import { Campaign } from './entities/campaign.entity';
 import { CampaignMember } from '../campaign-members/entities/campaign-member.entity';
+import { Scene } from '../scenes/entities/scene.entity';
+import { Item } from '../items/entities/item.entity';
+import { Character } from '../characters/entities/character.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Campaign, CampaignMember])],
+  imports: [TypeOrmModule.forFeature([Campaign, CampaignMember, Scene, Item, Character])],
   providers: [CampaignsService],
   controllers: [CampaignsController],
   exports: [CampaignsService],
